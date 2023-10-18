39ca1feec7d5816fd77d5ecc9d8e91cbc43bbcac
preshkelly71 <kelechi.precious.okafor@gmail.com>
2023-10-18 10:19:01 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -20,30 +20,3 @@ void reverse_array(int *a, int n)
 	a[n] = r;
 	}
 }
-
-
-
-Task 5
-
-#include "main.h"
-/**
- * string_toupper - main function
- *
- * @n: String input
- *
- * Return: dest
- */
-
-char *string_toupper(char *n)
-{
-	int i;
-
-	i = 0;
-	while (n[i] != '\0')
-	{
-	if (n[i] >= 'a' && n[i] <= 'z')
-	n[i] = n[i] - 32;
-	i++;
-	}
-return (n);
-}
