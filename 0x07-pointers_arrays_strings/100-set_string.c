2eb45323debb2c1bbf121e63e4ab0caf7287cce1
preshkelly71 <kelechi.precious.okafor@gmail.com>
2023-10-23 05:56:25 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - main function
+  *
+  * @f: Function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **f, char *to)
+
+{
+	*f = to;
+
+}
